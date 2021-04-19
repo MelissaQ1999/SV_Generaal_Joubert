f78ba39122fa6d0f54abe6cd437674cab1af15b1
MelissaQ1999 <61278416+MelissaQ1999@users.noreply.github.com>
2021-04-19 13:25:22 +0200

    Menu update

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,43 @@ $(window).on("load",function(){
     $("#loaderInner").fadeOut("slow");
     $("#doc-loader").delay(400).fadeOut("slow");
 });
+
+// ----- Hamburgermenu (de drie streepjes) ----- //
+$(document).ready(function(){
+	// menu click event
+	$('.menuBtn').click(function() {
+		$(this).toggleClass('act');
+			if($(this).hasClass('act')) {
+				$('.mainMenu').addClass('act');
+            }
+			else {
+				$('.mainMenu').removeClass('act');
+            }
+    });
+});
+
+// // Create a media condition that targets viewports at least 768px wide
+// const mediaQuery = window.matchMedia('(min-width: 1024px)')
+
+// // Check if the media query is true
+// if (mediaQuery.matches) {
+//     // Then trigger an alert
+//     $('.menuBtn').toggleClass('act');
+//         if($('.menuBtn').hasClass('act')) {
+//             $('.mainMenu').addClass('act');
+//         }
+// }
+
+
+// function myFunction(x) {
+//     x.classList.toggle('act');
+// }
+
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//  }
