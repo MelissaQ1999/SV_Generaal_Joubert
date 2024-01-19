327a3b7a06ed2183a3daf1cf4c38430197730d8f
MelissaQ1999 <61278416+MelissaQ1999@users.noreply.github.com>
2024-01-19 02:40:46 +0100

    mobile home animation succes
    Voor de mobile versie is het eindelijk gelukt om op de home pagina een universe animatie te maken. Voor de break points en andere pagina's kan nu deze stijl direct worden overgenomen!
    
    Verder kleine updates voor de navigatie structuur doorgevoerd.

--- scripts/main.js
+++ scripts/main.js
@@ -13,14 +13,79 @@ $(document).ready(function(){
 	$('.menuBtn').click(function() {
 		$(this).toggleClass('act');
 			if($(this).hasClass('act')) {
-                // $('html').css('overflow', 'hidden');
                 $('.mainMenu').addClass('act');
                 $('header').addClass('act');
+                $('a.logo').addClass('act');
             }
 			else {
-                // $('html').css('overflow', 'auto');
                 $('.mainMenu').removeClass('act');
                 $('header').removeClass('act');
+                $('a.logo').removeClass('act');
             }
     });
 });
+
+
+// ----- Voor het uitvoeren van de animaties door de website heen ----- //
+const observerN = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showN');
+         } 
+        // else {
+        //     entry.target.classList.remove('showN');
+        // }
+    });
+});
+
+const hiddenNElements = document.querySelectorAll('.hiddenN');
+hiddenNElements.forEach((el) => observerN.observe(el));
+
+
+const observerIMG = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showIMG');
+         } 
+        // else {
+        //     entry.target.classList.remove('showIMG');
+        // }
+    });
+});
+
+const hiddenIMGElements = document.querySelectorAll('.hiddenIMG');
+hiddenIMGElements.forEach((el) => observerIMG.observe(el));
+
+
+const observerSL = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showSL');
+         } 
+        else {
+            entry.target.classList.remove('showSL');
+        }
+    });
+});
+
+const hiddenSLElements = document.querySelectorAll('.hiddenSL');
+hiddenSLElements.forEach((el) => observerSL.observe(el));
+
+
+const observerSR = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showSR');
+         } 
+        else {
+            entry.target.classList.remove('showSR');
+        }
+    });
+});
+
+const hiddenSRElements = document.querySelectorAll('.hiddenSR');
+hiddenSRElements.forEach((el) => observerSR.observe(el));
